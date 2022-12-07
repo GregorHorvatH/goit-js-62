626c7b15195d2f41ddea3c86f9394b1471d87c7e
Herhel Horvat <herhel.horvat@teaminternational.com>
2022-12-07 20:56:03 +0200

    module 11.22

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('listening on *:', PORT);
+});
